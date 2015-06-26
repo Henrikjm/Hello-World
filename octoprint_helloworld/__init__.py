a45cc0a1fbc8cef37576dad2e6d61e5b8f826dd7
Henrikjm <melandhenrik@gmail.com>
2015-06-26 16:13:53 +0200

    Changed init file

--- octoprint_helloworld/__init__.py
+++ octoprint_helloworld/__init__.py
@@ -1,30 +1,13 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
-class HelloworldPlugin(octoprint.plugin.TemplatePlugin):
-	# TODO Implement me!
-	pass
+class HelloWorldPlugin(octoprint.plugin.StartupPlugin):
+    def on_after_startup(self):
+        self._logger.info("Hello World!")
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Helloworld Plugin"
-
-def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = HelloworldPlugin()
-
-	# global __plugin_hooks__
-	# __plugin_hooks__ = {
-	#    "some.octoprint.hook": __plugin_implementation__.some_hook_handler
-	# }
-
+__plugin_name__ = "Hello World"
+__plugin_version__ = "1.0.0"
+__plugin_description__ = "A quick \"Hello World\" example plugin for OctoPrint"
+__plugin_implementation__ = HelloWorldPlugin()
